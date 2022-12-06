4efab1817b07af47fea65130b73ce4ee28495408
Srikanth V S <sksrik17@gmail.com>
2022-12-06 17:11:09 +0530

    Initial commit of my components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
-import React, { Component } from "react";
+import { Container } from "@material-ui/core";
+import { Height } from "@material-ui/icons";
+import React from "react";
+import Header from "./components/Header";
+// import "../src/app.css";
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  backgroundcolor: {
+    background: "#4dabf5",
+    height: 575,
+    width: 1255,
+  },
+}));
 
 function App() {
-  return <div className="App"></div>;
+  const classes = useStyles();
+  return (
+    // <div style={{ backgroundColor: "red", height: "400px" }}>
+    <Container className={classes.backgroundcolor}>
+      <div>
+        <Header />
+      </div>
+    </Container>
+  );
 }
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import { AppBar, Typography } from "@material-ui/core";
+import React from "react";
+
+const Header = () => {
+  return (
+    // <div className="ui fixed menu">
+    //   <div className="ui container center">
+    <AppBar>
+      <Typography variant="h3">LOGO</Typography>
+    </AppBar>
+    //   </div>
+    // </div>
+  );
+};
+
+export default Header;
