6b740c333b7cb889d2dd90f947f51603c00e369a
Srikanth V S <sksrik17@gmail.com>
2022-12-07 15:33:21 +0530

    app,app-test,header and header-test components

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import React from "react";
 import Header from "./components/Header";
 // import "../src/app.css";
 import { makeStyles } from "@material-ui/core";
+// import classNames from "classnames";
 
 const useStyles = makeStyles((theme) => ({
   backgroundcolor: {
@@ -15,9 +16,14 @@ const useStyles = makeStyles((theme) => ({
 
 function App() {
   const classes = useStyles();
+  // const conclass = classNames("containerComponent", classes.backgroundcolor);
   return (
     // <div style={{ backgroundColor: "red", height: "400px" }}>
-    <Container className={classes.backgroundcolor}>
+    // <Container className={conclass}>
+    <Container
+      data-test="containerComponent"
+      className={classes.backgroundcolor}
+    >
       <div>
         <Header />
       </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,8 +5,10 @@ const Header = () => {
   return (
     // <div className="ui fixed menu">
     //   <div className="ui container center">
-    <AppBar>
-      <Typography variant="h3">LOGO</Typography>
+    <AppBar data-test="appbarComp">
+      <Typography data-test="logoComp" variant="h3">
+        LOGO
+      </Typography>
     </AppBar>
     //   </div>
     // </div>
